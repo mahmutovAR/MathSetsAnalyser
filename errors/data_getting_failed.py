29966797f29e53343b1b5587d382d08c3db573e2
mahmutovAR <armax045@gmail.com>
2022-11-23 23:25:08 +0400

    The plugin, run and test have been optimized. Trial script added.

--- errors/data_getting_failed.py
+++ errors/data_getting_failed.py
@@ -1,10 +1,9 @@
 class DataGettingError(Exception):
-    __slots__ = ['__data_id', '__error_id']
+    __slots__ = ['__error_id']
 
-    def __init__(self, data_id, error_id):
-        self.__data_id = data_id
+    def __init__(self, error_id):
         self.__error_id = error_id
-        self.__description = f"""Error! Failed to get data from: {self.__data_id}\n{self.__error_id}"""
+        self.__description = f"""Error! Failed to get initial math sets from data file:\n{self.__error_id}"""
 
     def __str__(self):
         return f'{self.__description}'
